3a8285c5b636497a610640a687572bf35266d067
Christopher Goodwin <goodwin.chris2021@gmail.com>
2023-09-15 18:21:03 -0500

    Added Shop page functionality and styling. Started to add dynamic routes to category pages in shop component

--- src/index.js
+++ src/index.js
@@ -4,22 +4,22 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import { UserProvider } from "./contexts/UserContext";
-import { ProductsProvider } from "./contexts/ProductsContext";
+import { CategoriesProvider } from "./contexts/CategoriesContext";
 import { CartProvider } from "./contexts/CartContext";
 
 import "./index.scss";
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
     <React.StrictMode>
         <BrowserRouter>
             <UserProvider>
-                <ProductsProvider>
+                <CategoriesProvider>
                     <CartProvider>
                         <App />
                     </CartProvider>
-                </ProductsProvider>
+                </CategoriesProvider>
             </UserProvider>
         </BrowserRouter>
     </React.StrictMode>
---
+++ src/routes/categories-preview/CategoriesPreview.jsx
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+
+import { CategoriesContext } from "../../contexts/CategoriesContext";
+import CategoryPreview from "../../components/category-preview/CategoryPreview";
+
+const CategoriesPreview = () => {
+    const { categoriesMap } = useContext(CategoriesContext);
+    return (
+        <>
+            {Object.keys(categoriesMap).map((key) => {
+                const products = categoriesMap[key];
+                return (
+                    <CategoryPreview
+                        key={key}
+                        title={key}
+                        products={products}
+                    />
+                );
+            })}
+        </>
+    );
+};
+
+export default CategoriesPreview;
