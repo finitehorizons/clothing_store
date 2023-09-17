2a4eacd3d38323dad109aa7ecb49577f71e9d5ae
Christopher Goodwin <goodwin.chris2021@gmail.com>
2023-09-16 20:57:02 -0500

    fixed compile errors

--- src/contexts/CategoriesContext.jsx
+++ src/contexts/CategoriesContext.jsx
@@ -1,6 +1,11 @@
 import { createContext, useState, useEffect } from "react";
 
-import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
+import SHOP_DATA from "../shop-data";
+
+import {
+    getCategoriesAndDocuments,
+    addCollectionAndDocuments,
+} from "../utils/firebase/firebase.utils";
 
 export const CategoriesContext = createContext({
     categoriesMap: {},
@@ -8,6 +13,10 @@ export const CategoriesContext = createContext({
 
 export const CategoriesProvider = ({ children }) => {
     const [categoriesMap, setCategoriesMap] = useState({});
+
+    useEffect(() => {
+        addCollectionAndDocuments("categories", SHOP_DATA);
+    }, []);
 
     useEffect(() => {
         const getCategoriesMap = async () => {
