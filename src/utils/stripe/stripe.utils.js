d5c251e1c8b268b5a6d1a3a3b9d446d57369a0e3
Christopher Goodwin <goodwin.chris2021@gmail.com>
2024-01-13 22:48:01 -0600

    Update stripe.utils.js
    securing with env

--- src/utils/stripe/stripe.utils.js
+++ src/utils/stripe/stripe.utils.js
@@ -1,5 +1,5 @@
 import { loadStripe } from "@stripe/stripe-js";
 
 export const stripePromise = loadStripe(
-    "pk_test_51O7isCLXlJTbYyLP6uHwSnvT8pP4hFf5pRuTiYdqMggpHEfGf227r5ma3WAz74lFkE5Fy87aKiAj3lNlJ370jvGR00K1pC7ZIk"
+    process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
 );
