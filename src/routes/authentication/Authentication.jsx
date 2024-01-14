152033687f65a2e9ca4bdb5b3beca7012ce559dd
Christopher Goodwin <goodwin.chris2021@gmail.com>
2024-01-13 22:47:37 -0600

    Update Authentication.jsx
    added demo account label

--- src/routes/authentication/Authentication.jsx
+++ src/routes/authentication/Authentication.jsx
@@ -1,14 +1,28 @@
 import SignInForm from "../../components/sign-in-form/SignInForm";
 import SignUpForm from "../../components/sign-up-form/SignUpForm";
 
-import { AuthContainer } from "./authentication.styles";
+import { AuthContainer, DemoWrapper } from "./authentication.styles";
 
+const Demo = () => {
+    return (
+    <DemoWrapper>
+        <h2>Demo Account</h2>
+        <p>Email: demo@demo.com</p>
+        <p>Password: 12341234</p>
+
+    </DemoWrapper>
+    )
+}
 const Authentication = () => {
     return (
+        <>
         <AuthContainer>
             <SignInForm />
             <SignUpForm />
         </AuthContainer>
+            <Demo />
+
+        </>
     );
 };
 
